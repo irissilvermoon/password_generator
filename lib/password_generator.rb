665153eac033ccf5a8f92f29fbf35f44b0165157
Karen Caplan <keaslk@gmail.com>
2012-12-09 20:22:55 -0800

    more passing tests plus working random password generator

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -1,14 +1,23 @@
 module PasswordGenerator
 
   CHARACTER_SET = "".tap do |string|
-    string << ('a'..'z').map.to_s
-    string << ('A'..'Z').map.to_s
-    string << (0..9).map.to_s
+    string << ('a'..'z').to_a.inject(:+)
+    string << ('A'..'Z').to_a.inject(:+)
+    string << (0..9).to_a.inject(:+)
     string << '!@#$%^&*()_+='
   end
 
   def self.generate_password(length = 20)
-    'd' * length
+    string = ''
+
+    length.times do |i| 
+      max_index = CHARACTER_SET.length - 1
+      index = rand(0..max_index)
+      character = CHARACTER_SET[index]
+      string << character
+    end
+
+    string
   end
 
 end
